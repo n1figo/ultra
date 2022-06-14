9b686f90a5816bd59455b4679ba17b7a3a8cdcb0
n1figo <n1figo@naver.com>
2022-06-14 18:05:34 +0900

    220614_ㅍ2

--- this_quarter.py
+++ this_quarter.py
@@ -34,28 +34,29 @@ class make_this_quarter_num:
         # print("Current date: ", todays_date)
             
         # fetching the current year, month and day of today
-        print("Current year:", todays_date.year)
+        print("현재연도는:", todays_date.year, '년 입니다.')
         # print("Current month:", todays_date.month)
         # print("Current day:", todays_date.day)
 
         조회대상분기 = quarter - 1
-        print(조회대상분기)
+        print('조회대상분기는', 조회대상분기, '입니다.')
 
-        """이전분기까지 yoy qoq 산출"""
-        selected_cols = [cols for cols in df_시총필터링_상위이십퍼센트_지주사제외_밸류순위.columns.tolist() if ('영업이익' in cols) or ('순이익' in cols)]
-        selected_cols_yoy_qoq = [cols for cols in selected_cols if ('YOY' in cols) or ('QOQ' in cols)]
-        current_year_quarter = str(int(todays_date.year))[2:] + '년' + str(조회대상분기) + 'Q'
-        print('현재연도및분기: ', current_year_quarter)  # 현재연도및분기:  21년3Q
-        selected_cols_yoy_qoq_current_before = [cols for cols in selected_cols_yoy_qoq if current_year_quarter in cols]
-        if '(E)' in selected_cols_yoy_qoq_current_before[0] :
-            selected_cols_yoy_qoq_current_before = str(int(todays_date.year))[2:] + '년' + str(조회대상분기-1) + 'Q'
-        print('확정현재연도및분기: ', selected_cols_yoy_qoq_current_before) # 확정현재연도및분기:  21년2Q
+        # """이전분기까지 yoy qoq 산출"""
+        # ---> make.py로 옮기기
+        # selected_cols = [cols for cols in df_시총필터링_상위이십퍼센트_지주사제외_밸류순위.columns.tolist() if ('영업이익' in cols) or ('순이익' in cols)]
+        # selected_cols_yoy_qoq = [cols for cols in selected_cols if ('YOY' in cols) or ('QOQ' in cols)]
+        # current_year_quarter = str(int(todays_date.year))[2:] + '년' + str(조회대상분기) + 'Q'
+        # print('현재연도및분기: ', current_year_quarter)  # 현재연도및분기:  21년3Q
+        # selected_cols_yoy_qoq_current_before = [cols for cols in selected_cols_yoy_qoq if current_year_quarter in cols]
+        # if '(E)' in selected_cols_yoy_qoq_current_before[0] :
+        #     selected_cols_yoy_qoq_current_before = str(int(todays_date.year))[2:] + '년' + str(조회대상분기-1) + 'Q'
+        # print('확정현재연도및분기: ', selected_cols_yoy_qoq_current_before) # 확정현재연도및분기:  21년2Q
 
-        """조정된 분기로 yoy qoq 산출"""
-        selected_cols_yoy_qoq_current_before_adjusted = [cols for cols in selected_cols_yoy_qoq if selected_cols_yoy_qoq_current_before in cols]
-        print('조정분기데이터:' , selected_cols_yoy_qoq_current_before_adjusted)
+        # """조정된 분기로 yoy qoq 산출"""
+        # selected_cols_yoy_qoq_current_before_adjusted = [cols for cols in selected_cols_yoy_qoq if selected_cols_yoy_qoq_current_before in cols]
+        # print('조정분기데이터:' , selected_cols_yoy_qoq_current_before_adjusted)
         
-        return selected_cols_yoy_qoq_current_before_adjusted
+        # return selected_cols_yoy_qoq_current_before_adjusted
 
 
 
